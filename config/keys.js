8ee87b54dc1ff69054da1b19246e02af6a5b3bc2
emre <test@example.com>
2018-03-01 23:45:04 -0600

    Create Google Strategy and Auth callback

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://emre:2710@ds023500.mlab.com:23500/storybooks-dev',
+    googleClientID: '486741287022-dhom9adnl911vp86frb5qgulhge72oo2.apps.googleusercontent.com',
+    googleClientSecret: '-Ta_5J030b5ySTV17_BENRRh' 
+}
