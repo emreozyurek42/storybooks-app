5c75bce3e02c1311fc2237a7ff6e4cae7c6784fb
emre <test@example.com>
2018-03-02 00:44:53 -0600

    Create the user model

--- app.js
+++ app.js
@@ -2,9 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+require('./models/User');
+
 require('./config/passport')(passport);
 
 const auth = require('./routes/auth');
+
+const keys = require('./config/keys');
+
+mongoose.connect(keys.mongoURI)
+.then(() => console.log('MongoDB connected'))
+.catch((err) => console.log(err));
 
 const app = express();
 
