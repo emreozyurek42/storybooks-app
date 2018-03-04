94cfcdd7ccea8af500cbab08c123e6b8f7affaff
emre <test@example.com>
2018-03-03 23:09:09 -0600

    Fetch and display public stories

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const passport = require('passport');
 
 require('./models/User');
+require('./models/Story');
 
 require('./config/passport')(passport);
 
@@ -16,13 +18,27 @@ const stories = require('./routes/stories');
 
 const keys = require('./config/keys');
 
+const {
+    truncate,
+    stripTags,
+    formatDate
+} = require('./helpers/hbs');
+
 mongoose.connect(keys.mongoURI)
 .then(() => console.log('MongoDB connected'))
 .catch((err) => console.log(err));
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.engine('handlebars', exphbs({
+    helpers: {
+      truncate: truncate,
+      stripTags: stripTags,
+      formatDate: formatDate
+    },
     defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
--- routes/stories.js
+++ routes/stories.js
@@ -1,14 +1,54 @@
 const express = require('express');
 const router= express.Router();
+const mongoose = require('mongoose');
+const Story = mongoose.model('stories');
+const User = mongoose.model('users');
 const {ensureAuthenticated, ensureGuest} = require('../helpers/auth');
 
 
 router.get('/', (req, res) => {
-    res.render('stories/index');
+    Story.find({status: 'public'})
+    .populate('user')
+    .then(stories => {
+        res.render('stories/index', {stories});
+    }); 
+});
+
+router.get('/show/:id', (req, res) => {
+    Story.findOne({
+        _id: req.params.id
+    })
+    .populate('user')
+    .then(story => {
+        res.render('stories/show', {story:story});
+    })
 });
 
 router.get('/add', ensureAuthenticated, (req, res) => {
     res.render('stories/add');
+});
+
+router.post('/', (req, res) => {
+ let allowCommnets;
+
+ if(req.body.allowCommnets) {
+     allowCommnets = true;
+ } else {
+     allowCommnets = false;
+ }
+ const newStory = {
+     title: req.body.title,
+     body: req.body.body,
+     status: req.body.status,
+     allowCommnets: allowCommnets,
+     user: req.user.id
+ }
+
+ new Story(newStory)
+ .save()
+ .then(story => {
+     res.redirect(`/stories/show/${story.id}`);
+ })
 });
 
 module.exports = router;
